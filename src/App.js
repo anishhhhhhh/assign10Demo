23ef9ca917c2dab13401a5740857c86d30e99781
FIRST_NAME LAST_NAME <ashtaputre.a@northeastern.edu>
2022-12-02 18:09:30 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-function App() {
+const apiKey = "7e645c5e4585ced4f48d0c5bcfc758c3";
+
+const App = () => {
+
+  const [name, setName] = useState();
+  const [data, setData] = useState([]);
+  const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+  const onSearch = (e) => {
+    const city = e.target.value;
+    setName(city);
+  }
+
+  const onSubmit = () => {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${apiKey}`)
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data[0].lat);
+        // console.log(data[0].lon);
+        // fetch(`https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${data[0].lat}&lon=${data[0].lon}&appid=${apiKey}`)
+        //   .then(res => res.json())
+        //   .then(data => {
+        //     console.log(data);
+        //   })
+
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=${apiKey}`)
+          .then(newRes => newRes.json())
+          .then(newData => {
+            setData(newData.list);
+          })
+        })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div className="header">
+          <input type='text' name='search' onChange={onSearch}/>
+          <button onClick={onSubmit}>Submit</button>
+          </div>
+          
+                <div className="weekDiv">
+                  {data.length != 0 && weekDays.map((day, index) => {
+                    return (
+                      <div key={index}>
+                      {/* <Route path={`/${day}`} component={Day}/> */}
+                      <p>{day}</p>
+                    </div>
+                  )
+                  })}
+                </div>
+
+        <div className="tempData">
+          {data.slice(0, 5).map((item, index) => {
+            return (
+              <div key={index}>
+                {console.log(item.weather[0].icon)}
+                <img src={"http://openweathermap.org/img/wn/" +item.weather[0].icon + "@2x.png"}></img>
+                <p>Max: {(item.main.temp_max - 273.15).toFixed(2)}°C</p>
+                <p>Min: {(item.main.temp_min - 273.15).toFixed(2)}°C</p>
+              </div>
+            )
+          })}
+        </div>
+        <div>
+        </div>
+      </div>
   );
 }
 
